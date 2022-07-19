111db3dbdaa7cd0c5d3167a6a4b49a06214db715
+ <+>
2022-07-19 12:01:41 -0400

    commit tree 0eebc7ee86cb6c849155117d1f08c67139440824

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const removeid = document.getElementById('main');
+removeid.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+const appendh1 = document.body.append(newHeader);
+
+
+
+ 
+
+
+
+
